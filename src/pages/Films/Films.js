fb10b74f1058c80396e43289d3a1b7eafaf472a7
disel-brest <disel-brest@mail.ru>
2020-09-11 17:48:58 +0300

    3.epam_sellfilms_api

--- src/pages/Films/Films.js
+++ src/pages/Films/Films.js
@@ -5,18 +5,37 @@ import FilmsList from '../../components/FilmsList';
 import { connect } from 'react-redux';
 import { fetchFilms } from '../../redux/actions';
 
-import { info } from '../../API/info';
+// import { info } from '../../API/info';
 
 class Films extends Component {
 
-    componentDidMount() {
-       this.props.fetchFilms(info)  //при загрузке страницы вызовится action(функция)   props? хз
+    // componentDidMount() {
+    //    this.props.fetchFilms(info)  //при загрузке страницы вызовится action(функция)   props? хз
+    // }
+
+    componentDidMount(){
+        fetch('http://localhost:7000/api/films', {     //fetch  импортировать не надо
+            method : 'GET'
+        })
+        .then(otvetservera => otvetservera.json())    // метод json() внутри себя преобр. данные(объект respons) что-то типа data.JSON.parse и возвращаем promis с объектом
+		.then(dannieservera => {
+            console.log(dannieservera);  
+            this.props.propsFetchFilms(dannieservera.response)    //.then(users)        получили массив 
+        })
+        .catch((error) => console.log(error));
     }
+
+    // componentDidMount() {
+    //     fetch('http://localhost:7000/api/films')
+    //       .then((response) => response.json())
+    //       .then((data) => this.props.fetchFilms(data.response))
+    //       .catch((error) => console.log(error));
+    // }
 
     render() {
 
         const { list } = this.props;
-        
+
         return (
             <div className='container'>
                 <section className="products-wrapper">
@@ -34,4 +53,4 @@ const mapStateToProps = (state)=>{         //глобальный state=store
     };
 }
 
-export default connect(mapStateToProps , { fetchFilms:fetchFilms })(Films);
+export default connect(mapStateToProps , { propsFetchFilms:fetchFilms })(Films);
